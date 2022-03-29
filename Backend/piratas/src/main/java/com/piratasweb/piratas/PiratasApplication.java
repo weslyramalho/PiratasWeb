e78ced88e345f2d9cc0a11a831b3c1d5c257674b
Wesly <weslyramalho@yahoo.com.br>
2022-03-29 19:18:05 -0300

    Projeto iniciado

---
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/PiratasApplication.java
@@ -0,0 +1,13 @@
+package com.piratasweb.piratas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratasApplication.class, args);
+	}
+
+}
---
+++ Backend/piratas/src/test/java/com/piratasweb/piratas/PiratasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piratasweb.piratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
