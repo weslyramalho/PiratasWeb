2d0dbfd726e796d1346aed47d7f87b37c83d6dff
Wesly <weslyramalho@yahoo.com.br>
2022-03-30 20:20:34 -0300

    mapeamento funcionario projeto

--- Backend/piratas/src/main/java/com/piratasweb/piratas/entities/Funcionario.java
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/entities/Funcionario.java
@@ -1,25 +1,50 @@
 package com.piratasweb.piratas.entities;
 
-public class Funcionario extends Pessoa {
-	private String cargo;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.piratasweb.piratas.entities.enums.Cargo;
+
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario extends Pessoa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Cargo cargo;
 	private String foto;
 	private Double salario;
 	private String senha;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "funcionario")
+	private List<Projeto> projetos = new ArrayList<>();
+	
 	public Funcionario() {
 		super();
 	}
-	public Funcionario(String nome, String email, String telefone, String cargo, String foto, Double salario, String senha) {
+	public Funcionario(String nome, String email, String telefone, Cargo cargo, String foto, Double salario, String senha) {
 		super(nome, email, telefone);
 		this.cargo = cargo;
 		this.foto = foto;
 		this.salario = salario;
 		this.senha = senha;
 	}
-	public String getCargo() {
+	public Cargo getCargo() {
 		return cargo;
 	}
-	public void setCargo(String cargo) {
+	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
 	}
 	public String getFoto() {
