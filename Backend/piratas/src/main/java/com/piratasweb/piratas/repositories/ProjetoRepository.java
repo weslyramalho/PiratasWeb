2fe4bcee03129f37e0839d27be49ae72bad829ca
Wesly <weslyramalho@yahoo.com.br>
2022-03-30 20:26:12 -0300

    projetoRepository criado

---
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/repositories/ProjetoRepository.java
@@ -0,0 +1,9 @@
+package com.piratasweb.piratas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.piratasweb.piratas.entities.Projeto;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Long>{
+
+}
