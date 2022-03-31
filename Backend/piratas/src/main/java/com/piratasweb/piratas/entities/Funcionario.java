2f9134c860bcf5586fa69cde9a282159715ea4dc
Wesly <weslyramalho@yahoo.com.br>
2022-03-31 20:13:47 -0300

    SecurytConfig

--- Backend/piratas/src/main/java/com/piratasweb/piratas/entities/Funcionario.java
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/entities/Funcionario.java
@@ -14,30 +14,72 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.piratasweb.piratas.entities.enums.Cargo;
 
-
-public class Funcionario extends Pessoa implements Serializable{
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
 	private Cargo cargo;
 	private String foto;
 	private Double salario;
 	private String senha;
 	
 
-	private List<Projeto> projetos = new ArrayList<>();
+	//private List<Projeto> projetos = new ArrayList<>();
 	
 	public Funcionario() {
-		super();
+
 	}
-	public Funcionario(String nome, String email, String telefone, Cargo cargo, String foto, Double salario, String senha) {
-		super(nome, email, telefone);
+	public Funcionario(Long id, String nome, String email, String telefone, Cargo cargo, String foto, Double salario, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
 		this.cargo = cargo;
 		this.foto = foto;
 		this.salario = salario;
 		this.senha = senha;
 	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	/*
+	public List<Projeto> getProjetos() {
+		return projetos;
+	}
+	public void setProjetos(List<Projeto> projetos) {
+		this.projetos = projetos;
+	} */
 	public Cargo getCargo() {
 		return cargo;
 	}
