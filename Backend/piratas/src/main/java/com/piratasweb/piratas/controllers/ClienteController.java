5d80013b0842e61192a68428111427b23ed28700
Wesly <weslyramalho@yahoo.com.br>
2022-03-30 21:18:24 -0300

    clienetcontroller findAll

--- Backend/piratas/src/main/java/com/piratasweb/piratas/controllers/ClienteController.java
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/controllers/ClienteController.java
@@ -1,5 +1,27 @@
 package com.piratasweb.piratas.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.piratasweb.piratas.entities.Cliente;
+import com.piratasweb.piratas.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
 public class ClienteController {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll(){
+		List<Cliente> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 
 }
