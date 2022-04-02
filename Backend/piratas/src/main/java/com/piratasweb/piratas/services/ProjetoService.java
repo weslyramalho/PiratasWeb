e5952a9dcd2393963848cb7288c38886cb201306
Wesly <weslyramalho@yahoo.com.br>
2022-04-02 15:36:17 -0300

    ProjetoService

---
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/services/ProjetoService.java
@@ -0,0 +1,34 @@
+package com.piratasweb.piratas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.piratasweb.piratas.entities.Cliente;
+import com.piratasweb.piratas.repositories.ClienteRepository;
+
+@Service
+public class ProjetoService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	public List<Cliente> findAll(){
+		return repository.findAll();
+	}
+	
+	public Optional<Cliente> findById(Long id) {
+		return repository.findById(id);
+
+	}
+	
+	public Cliente insert(Cliente obj) {
+		return repository.save(obj);
+	}
+	
+	
+	
+
+}
