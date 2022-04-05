8df145661065c231a68a21997666c32a791f8a95
Wesly <weslyramalho@yahoo.com.br>
2022-04-05 18:22:38 -0300

    adcionando exception em clienteService

--- Backend/piratas/src/main/java/com/piratasweb/piratas/services/ClienteService.java
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/services/ClienteService.java
@@ -3,11 +3,19 @@ package com.piratasweb.piratas.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.piratasweb.piratas.entities.Cliente;
 import com.piratasweb.piratas.repositories.ClienteRepository;
+import com.piratasweb.piratas.services.exceptions.DatabaseException;
+import com.piratasweb.piratas.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClienteService {
@@ -19,14 +27,46 @@ public class ClienteService {
 		return repository.findAll();
 	}
 	
-	public Optional<Cliente> findById(Long id) {
-		return repository.findById(id);
+	public Cliente findById(Long id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 
 	}
 	
 	public Cliente insert(Cliente obj) {
 		return repository.save(obj);
 	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Cliente uptadate(Long id, Cliente obj) {
+		try {
+			Cliente entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Cliente entity, Cliente obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setCnpj(obj.getCnpj());
+		entity.setLogo(obj.getLogo());
+		entity.setSite(obj.getSite());
+		entity.setTelefone(obj.getTelefone());
+	
+	}
+	
 	
 	
 	
