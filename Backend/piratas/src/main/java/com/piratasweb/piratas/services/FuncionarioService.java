bbc3c9649907b37167ced5fa2c7b401108441782
Wesly <weslyramalho@yahoo.com.br>
2022-04-07 10:53:54 -0300

    adcionado delete e update em funcionarioservice

--- Backend/piratas/src/main/java/com/piratasweb/piratas/services/FuncionarioService.java
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/services/FuncionarioService.java
@@ -3,11 +3,17 @@ package com.piratasweb.piratas.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.piratasweb.piratas.entities.Funcionario;
 import com.piratasweb.piratas.repositories.FuncionarioRepository;
+import com.piratasweb.piratas.services.exceptions.DatabaseException;
+import com.piratasweb.piratas.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -20,13 +26,42 @@ public class FuncionarioService {
 		return repository.findAll();
 	}
 	
-	public Optional<Funcionario> findById(Long id) {
-		return repository.findById(id);
+	public Funcionario findById(Long id) {
+		Optional<Funcionario> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 
 	}
 	
 	public Funcionario insert(Funcionario obj) {
 		return repository.save(obj);
+	}
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	public Funcionario uptadate(Long id, Funcionario obj) {
+		try {
+			Funcionario entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	private void updateData(Funcionario entity, Funcionario obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setTelefone(obj.getTelefone());
+		entity.setCargo(obj.getCargo());
+		entity.setFoto(obj.getFoto());
+		entity.setSalario(obj.getSalario());
+		entity.setSenha(obj.getSenha());
+	
 	}
 
 }
