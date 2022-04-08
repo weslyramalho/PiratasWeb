0e14d68f718407f6626f44900df7f53eb1076870
Wesly <weslyramalho@yahoo.com.br>
2022-04-08 18:24:30 -0300

    delete, insert e update

--- Backend/piratas/src/main/java/com/piratasweb/piratas/controllers/ClienteController.java
+++ Backend/piratas/src/main/java/com/piratasweb/piratas/controllers/ClienteController.java
@@ -1,12 +1,19 @@
 package com.piratasweb.piratas.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.piratasweb.piratas.entities.Cliente;
 import com.piratasweb.piratas.services.ClienteService;
@@ -22,6 +29,32 @@ public class ClienteController {
 	public ResponseEntity<List<Cliente>> findAll(){
 		List<Cliente> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Long id){
+		Cliente obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> insert(@RequestBody Cliente obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente obj){
+		obj = service.uptadate(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
