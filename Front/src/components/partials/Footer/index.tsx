6347a4cbcfbcba6681790a29ef720cb19bb1db87
Wesly <weslyramalho@yahoo.com.br>
2022-04-24 15:54:51 -0300

    Footer pronto

--- Front/src/components/partials/Footer/index.tsx
+++ Front/src/components/partials/Footer/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { FooterArea } from "./styled";
 
 
 const Footer =()=>{
     return(
-
-    )
+        <FooterArea>
+             Todos direitos reservados <br />
+             WRL Web
+        </FooterArea>
+       
+    );
 }
 
 export default Footer;
---
+++ Front/src/components/partials/Footer/styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterArea = styled.div`
+height: 100px;
+background-color: #C2DDC8;
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+color: #999;
+font-size: 14px;
+border-top: 1px  solid #CCC;
+margin-top: 20px;
+`
